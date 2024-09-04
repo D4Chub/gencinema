9d90d740ffbbbcc524192511ff60783496e5082d
D4CHub <akbievalim0578@gmail.com>
2024-09-04 16:29:35 +0300

    feat: add routers for movies and persons

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from rest_framework import routers
 
-from api.v1.movies.views import MovieListView, GenreListView, PersonListView, MovieDetailView, MovieGenresView, PersonMovieView
+from api.v1.movies.views import MovieViewSet, GenreListView, PersonViewSet, MovieDetailView, PersonMovieView
 
+routers = routers.DefaultRouter()
+routers.register('person', PersonViewSet, basename='person')
+routers.register('movie', MovieViewSet, basename='movie')
 
 urlpatterns = [
-    path('movie/', MovieListView.as_view(), name="movie_list"),
     path('genre/', GenreListView.as_view(), name="genre_list"),
     path('movie/<int:pk>/', MovieDetailView.as_view(), name="movie_detail"),
     path('actor/<int:actor_id>/movies/', PersonMovieView.as_view(), name="movies_by_actor"),
-]
+
+] + routers.urls
