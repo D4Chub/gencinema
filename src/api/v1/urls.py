b8db91d95c14966cd92f7288e5f3b69ab55f4b55
D4CHub <akbievalim0578@gmail.com>
2024-09-05 16:37:02 +0300

    feat(person): add endpoint to display all persons

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework import routers
 
-from api.v1.movies.views import MovieViewSet, GenreListView, PersonViewSet, MovieDetailView, PersonMovieView
+from api.v1.movies.views import MovieViewSet, GenreListView, PersonListView, MovieDetailView, PersonMovieView
 
 routers = routers.DefaultRouter()
-routers.register('person', PersonViewSet, basename='person')
 routers.register('movie', MovieViewSet, basename='movie')
 
 urlpatterns = [
     path('genre/', GenreListView.as_view(), name="genre_list"),
+    path('person/', PersonListView.as_view(), name="person_list"),
     path('movie/<int:pk>/', MovieDetailView.as_view(), name="movie_detail"),
     path('actor/<int:actor_id>/movies/', PersonMovieView.as_view(), name="movies_by_actor"),
 
